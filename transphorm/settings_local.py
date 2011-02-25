94f29b2c971543fc7e9237c8fc3bd2d71fa98053
stonerfuzz <caio.chewb@gmail.com>
2011-02-24 22:39:02 -0300

    Added twitter and facebook keys to auth.

--- transphorm/settings_local.py
+++ transphorm/settings_local.py
@@ -5,14 +5,19 @@ from os import path
 
 DATABASES = {
 	'default': {
-		'ENGINE': 'django.db.backends.mysql',
+		'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': 'transphorm',
-		'USER': 'transphorm',
-		'PASSWORD': 'iy4gkbV7JPwe40hj',
+		'USER': '',
+		'PASSWORD': '',
 		'HOST': '',
 		'PORT': '',
 	}
 }
 
-DEBUG = True
+TWITTER_CONSUMER_KEY     = 'BZyWOIVYcVuiZqiKq0JoSw'
+TWITTER_CONSUMER_SECRET  = '4SR2r8PNQ5JH4Dgpa6uYexxesDGBaZFRzOA72PFPeBI'
+#FACEBOOK_APP_ID          = ''
+#FACEBOOK_API_SECRET      = ''
+
+DEBUG = False
 SITE_ROOT = path.abspath(path.dirname(__file__) + '/../')
